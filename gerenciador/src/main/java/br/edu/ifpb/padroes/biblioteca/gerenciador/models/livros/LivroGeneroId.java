63bccf30b095bf3b324efd4d7c1b1b508eb78b3f
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-08-03 21:25:33 -0300

    feat: criando a classe LivroGenero

---
+++ gerenciador/src/main/java/br/edu/ifpb/padroes/biblioteca/gerenciador/models/livros/LivroGeneroId.java
@@ -0,0 +1,48 @@
+package br.edu.ifpb.padroes.biblioteca.gerenciador.models.livros;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class LivroGeneroId {
+    private Long livroId;
+    private Long generoId;
+
+    public LivroGeneroId() {
+    }
+
+    public LivroGeneroId(Long livroId, Long generoId) {
+        this.livroId = livroId;
+        this.generoId = generoId;
+    }
+
+    public Long getLivroId() {
+        return livroId;
+    }
+
+    public void setLivroId(Long livroId) {
+        this.livroId = livroId;
+    }
+
+    public Long getGeneroId() {
+        return generoId;
+    }
+
+    public void setGeneroId(Long generoId) {
+        this.generoId = generoId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LivroGeneroId that = (LivroGeneroId) o;
+        return Objects.equals(livroId, that.livroId) && Objects.equals(generoId, that.generoId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(livroId, generoId);
+    }
+}
